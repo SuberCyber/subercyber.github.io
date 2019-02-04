2abe222391d02486c8fd41c74f14cb678e206eb7
SuberCyber <43937156+SuberCyber@users.noreply.github.com>
2019-02-04 09:35:18 -0500

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,13 @@
 </head>
 <html>
 <body>
+	<div id="header">
+	<?php include("header.inc.php"); ?>
+	</div>
 <h1>Hello World</h1>
 <p>I'm hosted with GitHub Pages.</p>
+	<div id="footer">
+	<?php include("footer.inc.php"); ?>
+	</div>
 </body>
 </html>
